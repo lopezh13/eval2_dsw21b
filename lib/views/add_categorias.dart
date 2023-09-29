37b7d28b17c3214cfa7fc1ee0752d16e0345b3ef
LÓPEZ H <mario.lopez22@itca.edu.sv>
2023-09-29 12:55:16 -0600

    funciones al main y correcion de error en el add_categorias

--- lib/views/add_categorias.dart
+++ lib/views/add_categorias.dart
@@ -1,217 +1,112 @@
 import 'package:flutter/material.dart';
+import '../models/crud.dart';
 
-import '../models/crud.dart'; // Asegúrate de importar tu archivo CRUD personalizado
-
- 
-
-class EditCategoria extends StatefulWidget {
-
-  const EditCategoria({Key? key});
-
- 
+class AddCategoria extends StatefulWidget {
+  const AddCategoria({Key? key});
 
   @override
-
-  State<EditCategoria> createState() => _EditCategoriaState();
-
+  State<AddCategoria> createState() => _AddCategoriaState();
 }
 
- 
-
-class _EditCategoriaState extends State<EditCategoria> {
-
+class _AddCategoriaState extends State<AddCategoria> {
   final TextEditingController _idController = TextEditingController(text: "");
-
   final TextEditingController _nombreController = TextEditingController(text: "");
-
   final TextEditingController _estadoController = TextEditingController(text: "");
 
- 
-
   @override
-
   Widget build(BuildContext context) {
-
-    final Map arguments = ModalRoute.of(context)!.settings.arguments as Map;
-
- 
-
-    _idController.text = arguments['id']?? '';
-
-    _nombreController.text = arguments['nombre'];
-
-    _estadoController.text = arguments['estado'];
-
- 
-
     return Scaffold(
-
       appBar: AppBar(
-
-        title: const Text('Editando Categoría'),
-
+        title: const Text('Agregar Categoría'),
       ),
-
       body: Padding(
-
         padding: const EdgeInsets.all(15.0),
-
         child: Column(
-
           children: [
-
             const Text(
-
-              'Información de la Categoría',
-
+              'Datos de la Categoría',
               style: TextStyle(
-
                 fontSize: 30,
-
                 color: Colors.grey,
-
                 fontWeight: FontWeight.bold,
-
               ),
-
             ),
-
             const SizedBox(height: 20,),
-
             TextField(
-
               controller: _idController,
-
               keyboardType: TextInputType.text,
-
               decoration: const InputDecoration(
-
                 border: OutlineInputBorder(),
-
-                hintText: 'Digite el ID de la categoría',
-
-                prefixIcon: Icon(Icons.person, color: Colors.blue,),
-
+                hintText: 'Ingrese el ID de la categoría',
+                prefixIcon: Icon(
+                  Icons.person,
+                  color: Colors.blue,
+                ),
               ),
-
             ),
-
             const SizedBox(height: 20,),
-
             TextField(
-
               controller: _nombreController,
-
               keyboardType: TextInputType.text,
-
               decoration: const InputDecoration(
-
                 border: OutlineInputBorder(),
-
-                hintText: 'Digite el nombre de la categoría',
-
-                prefixIcon: Icon(Icons.category, color: Colors.red,),
-
+                hintText: 'Ingrese el nombre de la categoría',
+                prefixIcon: Icon(
+                  Icons.category,
+                  color: Colors.red,
+                ),
               ),
-
             ),
-
             const SizedBox(height: 20,),
-
             TextField(
-
               controller: _estadoController,
-
               keyboardType: TextInputType.text,
-
               decoration: const InputDecoration(
-
                 border: OutlineInputBorder(),
-
-                hintText: 'Digite el estado de la categoría',
-
-                prefixIcon: Icon(Icons.check_circle, color: Colors.green,),
-
+                hintText: 'Ingrese el estado de la categoría',
+                prefixIcon: Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                ),
               ),
-
             ),
-
             const SizedBox(height: 20,),
-
             ElevatedButton(
-
               onPressed: () async {
-
                 final id = _idController.text;
-
                 final nombre = _nombreController.text;
-
                 final estado = _estadoController.text;
 
- 
-
                 if (id.isNotEmpty && nombre.isNotEmpty && estado.isNotEmpty) {
-
-                  await editCategoria(id, nombre, estado).then((_) {
-
+                  await addCategoria(id, nombre, estado).then((_) {
                     Navigator.pop(context);
-
                   });
-
                 } else {
-
                   // Puedes agregar manejo de error si los campos están vacíos
-
                   showDialog(
-
                     context: context,
-
                     builder: (context) {
-
                       return AlertDialog(
-
                         title: Text('Campos Vacíos'),
-
                         content: Text('Por favor, complete todos los campos.'),
-
                         actions: [
-
                           TextButton(
-
                             onPressed: () {
-
                               Navigator.pop(context);
-
                             },
-
                             child: const Text('Aceptar'),
-
                           ),
-
                         ],
-
                       );
-
                     },
-
                   );
-
                 }
-
               },
-
-              child: const Text('Actualizar'),
-
+              child: const Text('Guardar'),
             ),
-
           ],
-
         ),
-
       ),
-
     );
-
   }
-
 }
